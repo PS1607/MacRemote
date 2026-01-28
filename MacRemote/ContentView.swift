45414304129537e7f91709bf1c133096b63c940a
Prakhar Sharma <prakharsharma1607@gmail.com>
2026-01-29 01:54:30 +0530

    Initial Commit

---
+++ MacRemote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacRemote
+//
+//  Created by Prakhar Sharma on 29/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacRemote/MacRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacRemoteApp.swift
+//  MacRemote
+//
+//  Created by Prakhar Sharma on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MacRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
